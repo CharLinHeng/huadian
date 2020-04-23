9e0e104202b2ccc28f27a32177d4c2f8c2c738a2
zhonghecheng <1446290705@qq.com>
2020-04-23 17:07:08 +0800

    change  Repair and improvement

--- sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/order/service/OrderService.java
+++ sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/order/service/OrderService.java
@@ -34,27 +34,29 @@ public class OrderService {
      */
     @Transactional(rollbackFor = Exception.class)
     public AppResponse addUserOrder(AddOrder addOrder){
-        //获取当前登入人的编号
-        addOrder.setUserCode(SecurityUtils.getCurrentUserId());
         //判断参数是否齐全
         if(null == addOrder.getCartCode() || addOrder.getCartCode() == ""){
             return AppResponse.paramError("购物车编号为空");
         }
+        //获取当前登入人的编号
+        addOrder.setUserCode(SecurityUtils.getCurrentUserId());
+        //将多个购物车编号转成待处理string数组
         List<String>cartCodeList = Arrays.asList(addOrder.getCartCode().split(","));
         String msg = "";
-        //生成随机编号
+        //生成 随机订单编号
         addOrder.setOrderCode(RandomCode.radmonkey());
         //初始化订单
         int result = orderDao.addUserOrder(addOrder);
         if(result > 0){
             msg = "初始化订单成功!";
             List<String>newCartCodeList = new ArrayList<>();
-            //查询有没有商品已经下架,返回已经下架的 购物车编号  已下架的商品无法购买
+            //查询购物车中 有没有商品已经下架或者已删除, 返回已经下架的 购物车编号 已下架的商品无法购买
             List<GoodsExpired> goodsOnShelves = orderDao.goodsExpired(cartCodeList);
             if(goodsOnShelves.size() > 0){
                 //将CartCodeList去除已过期的商品
                 msg = msg + "已经清除已下架的商品"+goodsOnShelves.size()+"个!";
                 boolean con;
+                //将已经下架的商品 从 购物车中清除
                 for(int i = 0; i < cartCodeList.size() ;i++){
                     con = true;
                     for(int j = 0;j < goodsOnShelves.size();j++){
@@ -69,41 +71,44 @@ public class OrderService {
                 }
                 //此时list中留下来的是没有过期的商品的购物车编号
             }
-            //查询待新增的 订单商品列表
+            //根据 购物车编号 查询待新增的 订单商品列表
             if(newCartCodeList.size() > 0){
                 //接下来，判断购物车中 其 每种商品的数量库存 是否 大于等于 购物车中的 每种商品数量 返回的是那些不符合数量的商品编号
                 List<GoodBigThanLibs> nonConformingProducts = orderDao.judgementStock(newCartCodeList);
                 if(nonConformingProducts.size() > 0){
-                    //将那些购物车的数量修改为当前商品库存的库存的最大数量，如果商品数量库存当前为0，也要替换。
+                    //将那些购物车的数量修改为当前商品库存的库存的最大数量，如果商品数量库存当前为0，购物车对应商品数量也要替换成 0。
                     String updateCodeList = "";
                     int index = 0;
                     String str = ",";
+                    //用逗号拼接上面经过过滤的 带插入到订单商品详情的购物车编号
                     for(GoodBigThanLibs goodBigThanLibs:nonConformingProducts){
                         if(index == 0){
                             str = "";
                         }
                         updateCodeList = updateCodeList+goodBigThanLibs.getGoodCode()+str;
                     }
+                    //去将购物车每件商品进行判断和更新，如果 某一件类型的商品数量大于商品库存，则  更新商品库存最大值
                     int updateNum = orderDao.updateGoodNum(Arrays.asList(updateCodeList.split(",")));
                     if(updateNum > 0){
                         msg = msg + "已更新部分购物车商品数量!";
                     }
                 }
-                //[默认支付成功] 生成订单后， 需要10分钟之内进行支付，否则 订单删除，商品库存恢复，如果支付成功，那么将纪录添加到顾客表
+                //生成订单后， 需要10分钟之内进行支付，否则 订单删除，商品库存恢复，
+                // 如果支付成功[订单 这里默认支付成功]，那么将纪录添加到顾客表，并且将 订单商品详情增加到对应表
                 List<Order> orderList = orderDao.queryOrderList(newCartCodeList);
                 for(int i = 0;i < orderList.size();i++){
                     //给每个订单商品详情随机生成订单详情编号
                     orderList.get(i).setOrderGCode(RandomCode.radmonkey());
                     orderList.get(i).setOrderCode(addOrder.getOrderCode());
                 }
-                //如果商品种类数量大于0， 那么 将购买的商品种类信息新增到订单详情
+                //如果 购物车内待购买的商品种类 数量大于0， 那么 将购买的商品种类信息新增到订单详情
                 if(orderList.size() > 0){
-                    //还要将 商品库存数量减少 指定购买的商品数量 然后售出数量 + 对应数量
+                    // 还要将 对应的 商品库存 数量减少 指定 购买的商品数量 然后 售出数量 + 对应数量
                     orderDao.updateGoodRelative(orderList);
-                    //新增到订单详情表  [这里漏了存商品image，需要补上]
+                    //新增到订单详情表  [这里漏了需要存商品的图片，需要补上]
                     result = orderDao.addOrderList(orderList);
                     //更新订单总价格  而且需要存 门店编号
-                    //
+                    //判断新增到 订单详情列表的 结果
                     if(result > 0){
                         //添加到顾客表 addOrder 和订单编号
                         Customer customer = orderDao.queryCustomerData(addOrder.getUserCode());
@@ -114,7 +119,7 @@ public class OrderService {
                         if(result > 0){
                             msg = msg + "新建客户成功!";
                         }
-                        //清空购物车
+                        //下单成功后，需要清空对应的购物车
                         int empty = orderDao.clearEmpty(cartCodeList);
                         if(empty > 0){
                             msg = msg + "已清空购物车!下单成功!";
